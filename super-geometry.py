1642a5c277d2f737e8cc79063d5e1753e9b41005
VaibhavSiroha <58651208+VaibhavSiroha@users.noreply.github.com>
2019-12-08 13:04:42 +0530

    added new program

---
+++ super-geometry.py
@@ -0,0 +1,19 @@
+import turtle
+import math
+import random
+sam=turtle.Turtle()
+sam.speed(0)
+sam.color("Red","Blue")
+sam.begin_fill()
+def square(turtle,size,angle):
+    for i in range(4):
+        turtle.forward(size)
+        turtle.left(angle)
+        turtle.forward(size)
+for a in range(4):
+    square(sam,100,90)
+    sam.penup()
+    sam.forward(100)
+    sam.pendown()
+sam.end_fill()
+turtle.done
