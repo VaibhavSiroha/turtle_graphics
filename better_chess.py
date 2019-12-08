2e04691f3bb47103defd0d3783c0e4193c03faf5
VaibhavSiroha <58651208+VaibhavSiroha@users.noreply.github.com>
2019-12-08 18:11:37 +0530

    this is better chess

---
+++ better_chess.py
@@ -0,0 +1,34 @@
+import turtle
+sam=turtle.Turtle()
+sam.speed(5)
+sam.setposition(100,0)
+sam.setposition(100,200)
+sam.right(90)
+for i in range(5):
+    sam.forward(400)
+    sam.right(90)
+    sam.forward(40)
+    sam.right(90)
+    sam.forward(400)
+    sam.left(90)
+    sam.forward(40)
+    sam.left(90)
+sam.forward(400)
+for i in range(5):
+    sam.left(90)
+    sam.forward(40)
+    sam.left(90)
+    sam.forward(40)
+    sam.left(90)
+    sam.forward(400)
+    sam.right(90)
+    sam.forward(40)
+    sam.right(90)
+    sam.forward(400)
+    sam.left(90)
+    sam.forward(40)
+    sam.right(90)
+    
+    
+
+turtle.done
