555297ff6eb6d98d761485bfbc72977a92b45386
VaibhavSiroha <58651208+VaibhavSiroha@users.noreply.github.com>
2020-01-19 15:10:15 +0530

    full snake game.py
    gg

--- snake.py
+++ snake.py
@@ -4,101 +4,179 @@ import random
 
 delay = 0.1
 
+# Score
+score = 0
+high_score = 0
+
+# Set up the screen
 wn = turtle.Screen()
+wn.title("naagin 5")
 wn.bgcolor("green")
-wn.setup(width=500,height=500)
-wn.tracer(0)
+wn.setup(width=600, height=600)
+wn.tracer(0) # Turns off the screen updates
 
-#snake head
-snake = turtle.Turtle()
-snake.shape("square")
-snake.color("black")
-snake.penup()
-snake.goto(0,0)
-snake.direction = "up"
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
-#snake food
+# Snake food
 food = turtle.Turtle()
+food.speed(0)
 food.shape("circle")
 food.color("red")
 food.penup()
-food.goto(0,90)
+food.goto(0,100)
 
-segment = []
+segments = []
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
 
-#move
-
+# Functions
 def go_up():
-	snake.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-	snake.direction = "down"	
-
-def go_right():
-	snake.direction = "right"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-	snake.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
-	if snake.direction == "up":
-		y = snake.ycor()
-		snake.sety(y+20)
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
 
-	if snake.direction == "down":
-		y = snake.ycor()
-		snake.sety(y-20)
-	
-	if snake.direction == "right":
-		x = snake.ycor()
-		snake.setx(x+20)
-		
-	if snake.direction == "left":
-		y = snake.ycor()
-		snake.setx(x-20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
 
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
 
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
-#keypad
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
 
-wn.listen
-wn.onkeypress(go_up, "t")
-wn.onkeypress(go_down, "v")
-wn.onkeypress(go_right, "h")
-wn.onkeypress(go_left, "d")
-
+# Main game loop
 while True:
-	wn.update()
-	if snake.distance(food) < 20 :
-		
-	#move the food
-		x = random.randint(-290,290)
-		y = random.randint(-290,290)
-		food.goto(x,y)
-		
-	#new segment
-	new_segment = turtle.Turtle()
-	new_segment.speed(0)
-	new_segment.shape("square")
-	new_segment.color("black")
-	new_segment.penup()
-	segment.append(new_segment)
-	
-#move new segment	
-for index in range(len(segments)-1, 0, -1):
-	x = segments[index-1].xcor()
-	y = segments[index-1].ycor()
-	segments[index].goto(x,y)
+    wn.update()
 
-#move segment 0 to whare the head is
-if len(segments) > 0:
-	x = snake.xcor()
-	y = snake.ycor()
-	segments[0].goto(x,y)		
-		
-	move()
-	
-	time.sleep(delay)
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()    
+
+    # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+        
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
 
 wn.mainloop()
